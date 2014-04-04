ae10954f1baeeefbd18f712db40c6cb8f8b85170
Joseph Garrone <jospeph.garrone@ensimag.imag.fr>
2014-04-04 09:42:02 +0200

    Voila

--- src/ensishell.c
+++ src/ensishell.c
@@ -5,15 +5,37 @@
  * Ce code est distribué sous la licence GPLv3+.     *
  *****************************************************/
 
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "variante.h"
 #include "readcmd.h"
 
+
+#include <stdlib.h> /*  Pour les constantes EXIT_SUCCESS et EXIT_FAILURE */
+#include <stdio.h> /*  Pour fprintf() */
+#include <unistd.h> /*  Pour fork(), exec */
+#include <errno.h> /*  Pour perror() et errno */
+#include <sys/types.h> /*  Pour le type pid_t */
+#include <sys/wait.h> /*  Pour wait() */
+
 #ifndef VARIANTE
 #error "Variante non défini !!"
 #endif
+
+
+/*  La fonction create_process duplique le processus appelant et retourne
+ *     le PID du processus fils ainsi créé */
+pid_t create_process(void)
+{
+	/*  On crée une nouvelle valeur de type pid_t */
+	pid_t pid;
+
+	/*  On fork() tant que l'erreur est EAGAIN */
+	do pid = fork(); while ((pid == -1) && (errno == EAGAIN));
+
+	/*  On retourne le PID du processus ainsi créé */
+	return pid;
+}
 
 int main() {
         printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
@@ -21,7 +43,7 @@ int main() {
 	while (1) {
 		struct cmdline *l;
 		int i, j;
-		char *prompt = "ensishell>";
+		char *prompt = "jojoLaCochone>";
 
 		l = readcmd(prompt);
 
@@ -50,5 +72,52 @@ int main() {
                         }
 			printf("\n");
 		}
+
+		char **cmd;
+		if( (cmd = l->seq[0]) != NULL ){
+			//cmd[0] nom du program
+			//cmd[1] premier argument...
+
+			pid_t pid = create_process();
+
+
+			switch (pid) {
+				/*  Si on a une erreur irrémédiable (ENOMEM dans notre cas) */
+				case -1:
+					perror("fork");
+					return EXIT_FAILURE;
+					break;
+					/*  Si on est dans le fils */
+				case 0:
+
+
+					/*  On crée un tableau contenant le nom du programme, l'argument et le 
+					 *         dernier "NULL" obligatoire  */
+
+					/*  On lance le programme  */
+					if (execvp(cmd[0], cmd) == -1) {
+						perror("execv");
+						return EXIT_FAILURE;
+					}
+
+					break;
+					/*  Si on est dans le père */
+				default:
+
+					if( !l->bg ){ 
+						if (wait(NULL) == -1) {
+							perror("wait :");
+							exit(EXIT_FAILURE);
+						}
+					}
+
+					break;
+			}
+
+
+
+		}
+
+
 	}
 }
